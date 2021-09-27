73f6785049e7f6790851b98d5ca3ea0a900b2a1e
Ryan Szczepanski <rszczepanski@comcast.net>
2021-09-26 20:40:06 -0400

    Added User Schema

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const ThoughtSchema = require("./Thought.js");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "First Name is Required",
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is Required",
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+
+    thoughts: [ThoughtSchema],
+
+    friends: [UserSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
